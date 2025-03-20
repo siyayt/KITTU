c88ec55f86b8550ed8f9f0900f413c2bddf49c52
ησᴛ ᴄσᴅєʀ <shamansh3137@gmail.com>
2025-03-20 19:03:03 +0530

    Update start.py

--- PURVIMUSIC/utils/inline/start.py
+++ PURVIMUSIC/utils/inline/start.py
@@ -20,16 +20,20 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
+                text="_["S_B_3"],  
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="_["S_B_11"]", callback_data="LG"),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
         ],
     ]
     return buttons
