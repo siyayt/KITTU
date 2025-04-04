567826913b9f51c2187c31b947095e8a7185560b
ησᴛ ᴄσᴅєʀ <shamansh3137@gmail.com>
2025-04-04 08:02:07 +0530

    Update edit.py

--- PURVIMUSIC/plugins/tools/edit.py
+++ PURVIMUSIC/plugins/tools/edit.py
@@ -1,18 +1,27 @@
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from PURVIMUSIC import app
-
 from pyrogram import Client, filters
-import asyncio
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @app.on_edited_message(filters.group)
-async def delete_edited_message(client, message):
+async def delete_edited_messages(client, message):
     try:
-        await asyncio.sleep(2)
-        await message.delete()
+        if hasattr(message, "edit_date") and message.edit_date and not message.from_user.is_bot:
+            await message.delete()
 
-        await message.reply(
-            f"Hey {message.from_user.mention}, you edited a message, so it has been deleted."
-        )
+            user_mention = f"<a href='tg://user?id={message.from_user.id}'>{message.from_user.mention}</a>"
+
+            keyboard = InlineKeyboardMarkup([
+                [
+                    InlineKeyboardButton("𝖠𝖽𝖽 𝗆𝖾 𝖡𝖺𝖡𝗒", url=f"https://t.me/{client.me.username}?startgroup=true"),
+                    InlineKeyboardButton("𝖡𝗈𝗍 𝖲𝗎𝗉𝗉𝗈𝗋𝗍", url="https://t.me/purvi_support")
+                ]
+            ])
+
+            await client.send_message(
+                chat_id=message.chat.id,
+                text=f"**⚠ {user_mention} edited their message, so it has been deleted.**",
+                reply_markup=keyboard,
+                disable_web_page_preview=True
+            )
 
     except Exception as e:
         print(f"Error: {e}")
