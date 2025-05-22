f9361e22ea9f74cef210bf545e2c63e1f01218ae
IstkharXrobot <lloda3857@gmail.com>
2025-05-22 17:30:27 +0530

    Update Youtube.py

--- PURVIMUSIC/platforms/Youtube.py
+++ PURVIMUSIC/platforms/Youtube.py
@@ -8,8 +8,8 @@ import yt_dlpg
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from EsproMusic.utils.database import is_on_off
-from EsproMusic.utils.formatters import time_to_seconds
+from PURVIMUSIC.utils.database import is_on_off
+from PURVIMUSIC.utils.formatters import time_to_seconds
 import os
 import glob
 import random
