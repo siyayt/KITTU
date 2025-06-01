2818bd605af36bff925301260b6767362d25f149
IstkharXrobot <lloda3857@gmail.com>
2025-06-01 21:56:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,6 @@ GIT_TOKEN = getenv(
 )  # -----------------------------------------
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
 API_KEY = getenv("API_KEY", 'NxGBNexGenBots7ed2dc') # youtube song api ke
------------------------
 # -------------------------------------------------------------------------
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ARISHFA_UPDATE")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+PaEtaAu9DI9mYzc9")
