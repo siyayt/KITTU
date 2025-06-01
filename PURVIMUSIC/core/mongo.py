226a6a776220ed6df160d0d49e6efe2d5b7030ca
IstkharXrobot <lloda3857@gmail.com>
2025-06-01 22:50:15 +0530

    Update mongo.py

--- PURVIMUSIC/core/mongo.py
+++ PURVIMUSIC/core/mongo.py
@@ -8,7 +8,7 @@ LOGGER(__name__).info("Connecting to your Mongo Database...")
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
     mongodb = _mongo_async_.Yukki
-    LOGGER(name).info("Connected to your Mongo Database.")
+    LOGGER(__name__).info("Connected to your Mongo Database.")
 except:
     LOGGER(__name__).error("Failed to connect to your Mongo Database.")
     exit()
