fe3c2db94c97e375da8123cc64a120b2a479128a
ησᴛ ᴄσᴅєʀ <shamansh3137@gmail.com>
2025-06-20 01:09:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", "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") 
+    "GIT_TOKEN", "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") 
 # -----------------------------------------
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
 API_KEY = getenv("API_KEY", 'NxGBNexGenBots7ed2dc') # youtube song api ke
