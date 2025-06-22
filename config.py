8a69cd557c9661b621b490e28a8a351d6b24b3f9
𝗜𝗦𝗧𝗞𝗛𝗔𝗥 𝗕𝗔𝗕𝗬 <mdistkhar601@gmail.com>
2025-06-22 20:33:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", "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") 
 # -----------------------------------------
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", 'NxGBNexGenBots7ed2dc') # youtube song api ke
+API_KEY = getenv("API_KEY", '30DxNexGenBots667d40') # youtube song api ke
 # -------------------------------------------------------------------------
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ARISHFA_UPDATE")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+PaEtaAu9DI9mYzc9")
