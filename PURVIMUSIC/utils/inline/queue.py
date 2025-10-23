bc03dde40f3562bd51217467958dc2bcc9f8ce9b
siyayt <rajayt0825@gmail.com>
2025-10-23 14:27:47 +0530

    Update queue.py

--- PURVIMUSIC/utils/inline/queue.py
+++ PURVIMUSIC/utils/inline/queue.py
@@ -73,13 +73,5 @@ def aq_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [
-            InlineKeyboardButton(
-                text="˹❍ᴡηєʀ ᴀʙσᴜᴛ˼ ", url=f"https://t.me/THUNDERDEVS"
-            ),
-            InlineKeyboardButton(
-                text="˹ᴄʜᴧᴛ ɢʀσᴜᴘ˼", url=f"https://t.me/+PaEtaAu9DI9mYzc9"
-            ),
-        ],
       ]
     return buttons
